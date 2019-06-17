0d8a096dbfe94fec8ad05f15f96e56aab55419b8
Jesús Ángel Pérez-Roca Fernández <yisus82@users.noreply.github.com>
2019-06-17 18:36:35 +0200

    Customers table created

---
+++ bankmanager-web/src/app/main/main-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService } from 'ontimize-web-ngx';
+
+import { MainComponent } from './main.component';
+import { HomeModule } from './home/home.module';
+import { CustomersModule } from './customers/customers.module';
+
+export function loadHomeModule() {
+  return HomeModule;
+}
+
+export function loadCustomersModule() {
+  return CustomersModule;
+}
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    canActivate: [AuthGuardService],
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {
+        path: 'home',
+        loadChildren: loadHomeModule
+      },
+      {
+        path: 'customers',
+        loadChildren: loadCustomersModule
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MainRoutingModule {}
