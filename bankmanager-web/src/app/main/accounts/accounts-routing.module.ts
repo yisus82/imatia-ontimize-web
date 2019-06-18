b295ec88bf8efc400ea6ab952758de2518f2dac9
Jesús Ángel Pérez-Roca Fernández <yisus82@users.noreply.github.com>
2019-06-18 11:08:49 +0200

    Accounts Detail + Accounts New

--- bankmanager-web/src/app/main/accounts/accounts-routing.module.ts
+++ bankmanager-web/src/app/main/accounts/accounts-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountsHomeComponent } from './accounts-home/accounts-home.component';
+import { AccountsDetailComponent } from './accounts-detail/accounts-detail.component';
+import { AccountsNewComponent } from './accounts-new/accounts-new.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AccountsHomeComponent
+  },
+  {
+    path: 'new',
+    component: AccountsNewComponent
+  },
+  {
+    path: ':ACCOUNTID',
+    component: AccountsDetailComponent
   }
 ];
 
--- bankmanager-web/src/app/main/accounts/accounts.module.ts
+++ bankmanager-web/src/app/main/accounts/accounts.module.ts
@@ -6,9 +6,11 @@ import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { AccountsHomeComponent } from './accounts-home/accounts-home.component';
 import { AccountNumberRenderComponent } from './accounts-home/account-number-render/account-number-render.component';
 import { SharedModule } from 'app/shared/shared.module';
+import { AccountsDetailComponent } from './accounts-detail/accounts-detail.component';
+import { AccountsNewComponent } from './accounts-new/accounts-new.component';
 
 @NgModule({
   imports: [CommonModule, OntimizeWebModule, SharedModule, AccountsRoutingModule],
-  declarations: [AccountsHomeComponent]
+  declarations: [AccountsHomeComponent, AccountsDetailComponent, AccountsNewComponent]
 })
 export class AccountsModule {}
